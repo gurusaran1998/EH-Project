8a979875b338c798fe48c1024667a8a1bd33b5b2
gurusaran1998 <gurusarandasari1998@gmail.com>
2020-12-14 00:20:31 +0530

    welcome

---
+++ Project/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="input-group">
+    	<p>Welcome </p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+</div>
+		
+</body>
+</html>
